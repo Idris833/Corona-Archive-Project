b0f5cde11bd72979891ad9f7700560237211c888
Idris Chendid <73201891+Idris833@users.noreply.github.com>
2022-09-27 14:10:25 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,214 @@
+# SE-Sprint01-Team32
+
+# README for Corona Archive
+
+### Contributors
+
+Sprint 1
+
+- Abel Berhane Woldemariam
+- Anastasiia Skryzhadlovska
+
+Sprint 2
+
+- Idris Chendid
+- Ilayda Güner
+
+## About the Project
+
+Corona Archive is a web service for tracking, displaying, evaluating, and
+archiving of the Corona infections in a particular location. The web service will be accessible to
+4 types of users:
+
+1. Visitors: Citizens or visitors will use the web service to indicate whether they have
+   entered a particular place and when they have done so.
+2. Places: Places which are frequently visited by people, such as clubs, pubs, restaurants,
+   cinemas etc. will use the web service to get access to a QR code, which uniquely
+   identifies their place. Citizens will scan this QR code to record their presence at that
+   place.
+3. Agency: The agency or the evaluation client will use the web service to generate coronarelated reports by collecting data from the database.
+4. Hospital: Hospitals will use the web service to mark people as infected and track anyone else that has been in contact with an infected person.
+
+
+### Built with
+
+- HTML
+- [Flask](https://www.fullstackpython.com/flask.html)
+- [Python3](https://www.python.org/download/releases/3.0/)
+- [MySQL](https://www.mysql.com/)
+- CSS
+- [Bootstrap](https://getbootstrap.com/docs/3.4/css/)
+
+## File Structure
+
+```
+\--SE-Sprint01-Team32
+        \--static
+            \--css                  # All the CSS Files used
+            \--imgages              # All the images used
+        \--templates
+            \--                     # Main HTML files
+        \--sql
+            \--                     # SQL Query used for initialization
+        \--tests
+            |--test_sprint1.py      # Main Testing Python Code
+        -- app.py                   # Main Python Code
+        -- README.md
+        -- requirements.txt         # Required flask dependencies to run this program
+        -- .gitignore               # Git ignored files included here
+        -- db.yaml                  # MySQL Database credentials included here 
+```
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. What things you need to install the software and how to install them.
+
+### Prerequisites
+
+- [Mysql](https://dev.mysql.com/downloads/installer/)
+- Flask
+
+```
+pip3 install Flask
+```
+
+- Virtual Env
+
+```
+sudo pip3 install virtualenv
+```
+
+## Installation Guide
+
+```bash
+# Clone the repo.
+
+# git clone https://github.com/Magrawal17/SE-Sprint01-Team32.git
+cd SE-Sprint01-Team32/
+
+# Create virtual environment
+$ virtualenv env
+
+# Start virtual environment
+$ source env/bin/activate
+
+# Install all the dependencies
+$ pip3 install -r requirements.txt
+
+# Open  MySQL
+$ mysql -u {ENTER YOUR USERNAME OR ROOT} -p
+
+# Run this command in MYSQL command line to create required database.
+mysql> source sql/se_corona.sql
+mysql> exit
+
+# Create db.yam file
+$ touch db.yaml
+
+# Open db.yaml and enter database credentials in the file format described below
+$ nano db.yaml
+
+# Run python server
+$ python3 app.py
+
+when running app.py add /home to start surfing on the website
+```
+
+### `db.yaml` file Format. Enter your respective credentials
+
+The production process was done on a local MySQL server. This may may work
+in CLAMV, however the local server is suggested. 
+
+```yaml
+mysql_host: "localhost"
+mysql_user: "{YOUR USERNAME IN YOUR COMPUTER}"
+mysql_password: "{YOUR PASSWORD IN YOUR COMPUTER}"
+mysql_db: "seteam32"
+```
+
+## Default Hospital and Agent Accounts Created for grading purposes
+
+```
+Agent
+username: Idris
+password: 12345678
+
+username: John
+password: asdas1221
+
+Hospital
+username: Melina
+password: 12345asd
+
+```
+
+# View Documentation
+
+Go to this URL once the server has started. It contains the documentation
+of the Flask API routes.
+
+```
+http://localhost:5000/docs  
+
+Add /home to access to the website
+```
+
+# Run tests
+
+Run this code once you are in the environment to execute the unit-testcases
+
+```sh
+$ python3 test_sprint2.py
+```
+
+## Sprint 1 Progress achieved
+
+✅ Created requirements.txt file, which includes all the required installation.
+
+✅ Added Hospital login functionality.
+
+✅ Added Agent login functionality.
+
+✅ Added Visitor Registeration functionality.
+
+✅ Added Place Owner Registeration functionality.
+
+✅ Added Profile display and edit data for visitor account.
+
+✅ Added Profile display for Hospital, Agents and Place owners.
+
+✅ Added QR Code display for place owner to track visitors.
+
+✅ QR code resizeability implemented
+
+✅ Added List of Visitors for Agent Account.
+
+✅ Added project installation guide to README.
+
+✅ Added db.yaml to store database credentials.
+
+✅ Created documentation generation infrastructure using Flask-Selfdoc.
+
+✅ Added 19 unittests that should all pass on the first run. 
+
+## Sprint 2 Progress achieved
+
+✅ Fixed bugs related to agent login (sql syntax problem)
+
+✅ Fixed bugs related to visitor registration (sql syntax problem)
+
+✅ Added a feature for the agent so that he can view the list of the visitors and a hospital button for the list of hospitals
+
+✅ Added a filter to the agent so that he can search for the visitors by name or adress or select all(write nothing in the search field just press "search") to see all the visitors
+
+✅ Added a confirm button which directs to a confirmation page (added by us: success2.html) for the place owner when he scans the QR code successfully
+
+✅ Fixed 2 test cases test_login_hospital/agent_account_success which failed by putting the right username
+
+✅ Improved the user interface and added another "agent" into the database for grading criteria (Idris, 12345678)
+
+✅ Organized the file structure by adding some files in gitignore and put the test cases python file outside the folder as a sperate file in order to be run easily in Windows.
+
+## Acknowledgments
+
+- Special thanks to everyone who helped during the first sprint developement.
---
+++ test_sprint2.py
@@ -0,0 +1,151 @@
+from app import app
+import unittest
+
+
+class FlaskTestCase(unittest.TestCase):
+
+    # First run: Working Test Cases - 18 . Failed Test Cases 2
+
+
+    # The login page opens successfully
+    def test_login_to_role_page(self):
+        tester = app.test_client(self)
+        response = tester.get('/login', content_type="html/text")
+        self.assertIn(b'Login Page', response.data)
+
+    # The registeration page opens successfully
+    def test_register_to_role_page(self):
+        tester = app.test_client(self)
+        response = tester.get('/register', content_type="html/text")
+        self.assertIn(b'Registration Page', response.data)
+
+    # The Hospital login page opens successfully
+    def test_login_Hospital_role(self):
+        tester = app.test_client(self)
+        response = tester.post(
+            '/sign-in', data=dict(roles="Hospital"), follow_redirects=True)
+        self.assertIn(b'Hospital\'s Login', response.data)
+
+    # The Agent login page opens successfully
+    def test_login_agent_role(self):
+        tester = app.test_client(self)
+        response = tester.post(
+            '/sign-in', data=dict(roles="Agent"), follow_redirects=True)
+        self.assertIn(b'Agent\'s Login', response.data)
+
+    # The Visitor registeration page opens successfully
+    def test_register_visitor_role(self):
+        tester = app.test_client(self)
+        response = tester.post(
+            '/sign-up', data=dict(roles="Visitor"), follow_redirects=True)
+        self.assertIn(b'Visitor\'s Registration', response.data)
+
+    # The Place Owner registeration page opens successfully
+    def test_register_place_owner_role(self):
+        tester = app.test_client(self)
+        response = tester.post(
+            '/sign-up', data=dict(roles="Place Owner"), follow_redirects=True)
+        self.assertIn(b'Place Owner\'s Registration', response.data)
+
+    def test_login_hospital_account_success(self):
+        tester = app.test_client(self)
+        response = tester.post(
+            '/authenticate', data=dict(hospitalUsername="Melina", hpass="12345asd"), follow_redirects=True)
+        self.assertIn(b'Hospital\'s Home Page', response.data)
+
+    def test_login_agent_account_success(self):
+        tester = app.test_client(self)
+        response = tester.post(
+            '/authenticate', data=dict(agentUsername="John", apass="asdas1221"), follow_redirects=True)
+        self.assertIn(b'Agent\'s Home Page', response.data)
+
+    def test_login_agent_account_unsuccess(self):
+        tester = app.test_client(self)
+        response = tester.post(
+            '/authenticate', data=dict(agentUsername="dfg", apass="asfsw234"), follow_redirects=True)
+        self.assertIn(b'Invalid Credentials! Please Try again', response.data)
+
+    # invalid credentials for hospital account login
+    def test_login_hospital_account_unsuccess(self):
+        tester = app.test_client(self)
+        response = tester.post(
+            '/authenticate', data=dict(hospitalUsername="sad", hpass="asfdsd5654"), follow_redirects=True)
+        self.assertIn(b'Invalid Credentials! Please Try again', response.data)
+
+    def test_register_place_acount_success(self):
+        tester = app.test_client(self)
+        response = tester.post(
+            '/register/owner', data=dict(placeName="something 36", placeAddress="somewhere 83"), follow_redirects=True)
+        self.assertIn(b'QR for:', response.data)
+
+    # this test will fail after the first time you run the tests because of duplicate
+    def test_register_visitor_acount_success(self):
+        tester = app.test_client(self)
+        response = tester.post(
+            '/register/visitor', data=dict(name="something 6", address="somewhere 8", number="0123223343", email="asd@sd.co"), follow_redirects=True)
+        self.assertIn(b'QR Scanner Page', response.data)
+
+    # invalid input for visitor account login
+    def test_register_visitor_acount_unsuccess(self):
+        tester = app.test_client(self)
+        response = tester.post(
+            '/register/visitor', data=dict(name="something 6", address="somewhere 8", number="0123223343", email="asd@sd.co"), follow_redirects=True)
+        self.assertIn(b'Unable to register! Please Try again.', response.data)
+
+    def test_profile_Agent_role(self):
+        with app.test_client() as testClient:
+            with testClient.session_transaction() as session:
+                session['username'] = "asd"
+        response = testClient.get(
+            '/profile', content_type="html/text")
+        self.assertIn(b'Hello, asd', response.data)
+
+    def test_profile_Hospital_role(self):
+        with app.test_client() as testClient:
+            with testClient.session_transaction() as session:
+                session['username'] = "das"
+        response = testClient.get(
+            '/profile', content_type="html/text")
+        self.assertIn(b'Hello, das', response.data)
+
+    def test_profile_visitor_role(self):
+        with app.test_client() as testClient:
+            with testClient.session_transaction() as session:
+                session["name"] = "John"
+                session['email'] = "jhn@gm.co"
+                session['number'] = "0123432644"
+                session['address'] = "college ring 9"
+        response = testClient.get(
+            '/profile', content_type="html/text")
+        self.assertIn(b'Hello, John', response.data)
+
+    def test_profile_place_role(self):
+        with app.test_client() as testClient:
+            with testClient.session_transaction() as session:
+                session["place_name"] = "Rockies"
+        response = testClient.get(
+            '/profile', content_type="html/text")
+        self.assertIn(b'Hello, Rockies', response.data)
+
+    # visitor profile data update success
+    def test_visitor_profile_update_success(self):
+        tester = app.test_client(self)
+        response = tester.post(
+            '/update/profile', data=dict(name="something 6", address="newplace 8", number="01234444445", email="asd@sd.co"), follow_redirects=True)
+        self.assertIn(b'Profile Updated!', response.data)
+
+    # logout from the web-app successfully
+    def test_logout_from_account_page(self):
+        tester = app.test_client(self)
+        response = tester.get(
+            '/logout', content_type="html/text", follow_redirects=True)
+        self.assertIn(b'Login Page', response.data)
+
+    # The success page opens successfully
+    def test_success_page(self):
+        tester = app.test_client(self)
+        response = tester.get('/success2', content_type="html/text")
+        self.assertIn(b'Success Page', response.data)
+
+if __name__ == '__main__':
+    unittest.main()
